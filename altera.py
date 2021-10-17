44a4b3a4a97459caeffad4731d81add48ccced70
Matheus Henrique <monteiro.matheus.789@hotmail.com>
2021-10-17 14:14:49 -0300

    remove alteração

--- altera.py
+++ altera.py
@@ -1,3 +1,3 @@
 print("Essa é uma alteração no remoto! \nEssa é uma alteração no git flow\nE pronto!" )
 
-print("Oi, está é uma alteração feita no release\n new")
+print("Oi, está é uma alteração feita no release")
