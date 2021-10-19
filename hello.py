3601c1d43541ee92a50e3ce26dbb4f4ff507b345
n11n0s <vinicius.rafael751@gmail.com>
2021-10-18 21:06:12 -0300

    modificando o hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
+import time
 print('Hello World')
 print('Testando o push')
+hello_world = True
+quantidade_HW = int(input("quantos ' Hello World's ' você quer que apareça ? \n"))
+contador_HW = 0 
+while hello_world : 
+    time.sleep(1)
+    print("Hello World")
+    contador_HW += 1
+    if contador_HW == quantidade_HW:
+        hello_world = False
