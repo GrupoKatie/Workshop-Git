1d948375f851ce2c8aea4e7000a4f3ba6244538a
n11n0s <vinicius.rafael751@gmail.com>
2021-10-18 22:43:26 -0300

    adicionando funcionalidade 1

--- hello.py
+++ hello.py
@@ -1,12 +1,13 @@
 import time
 print('Hello World')
 print('Testando o push')
-hello_world = True
-quantidade_HW = int(input("quantos ' Hello World's ' você quer que apareça ? \n"))
-contador_HW = 0 
-while hello_world : 
-    time.sleep(1)
-    print("Hello World")
-    contador_HW += 1
-    if contador_HW == quantidade_HW:
-        hello_world = False
+def funcionalidade_1(quantidade_HW):
+    hello_world = True
+    contador_HW = 0 
+    while hello_world : 
+        time.sleep(1)
+        print("Hello World")
+        contador_HW += 1
+        if contador_HW == quantidade_HW:
+            hello_world = False
+funcionalidade_1(25)
