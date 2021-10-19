a7bdc7b77bedd5241e3be9427d9080ceaec075d5
Flavia Soares <flaviasoares@ic.ufal.br>
2021-10-18 22:11:50 -0300

    adicionada função Hello no  hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
+def Hello():
+    nome = input()
+    print(f"Hello, {nome}!")
+
+Hello()
+
 print('Hello World')
 print('Testando o push')
 print('Esse curso de git tá muito massa! Aprendi muita coisa! Vocês são muito top!')
+
